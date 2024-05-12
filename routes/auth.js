ead619d650a23e128e47cc286a80e961030aba61
Sourabh Aggarwal <devsourabhagg@gmail.com>
2024-05-12 11:39:58 +0530

    validator added

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { query, validationResult, body } = require('express-validator');
 
 
 //create a user using post "/api/auth" -> This API does not require auth
 
-router.post('/createuser',(req,res)=>{
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
+router.post('/createuser',[
+   body('name','Enter a valid name').isLength({min:3}),
+   body('email','Enter a valid email').isEmail(),
+   body('password').isLength({min:5}),
+],(req,res)=>{
+  // console.log(req.body);
+  // const user = User(req.body);
+  // user.save();
+  // res.send(req.body);
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors : errors.array()});
+  }
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(sourabh => res.json(sourabh))
+    .catch(err => {console.log(err)
+    res.json({error: 'Please Enter a unique value for email',message : err.message})}
+    );
 
 })
 
