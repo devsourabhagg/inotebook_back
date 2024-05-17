7a6a1d43562b883983c1ff84cfce48775aebb6a6
Sourabh Aggarwal <devsourabhagg@gmail.com>
2024-05-17 23:30:19 +0530

    final changes done

--- routes/auth.js
+++ routes/auth.js
@@ -10,23 +10,38 @@ router.post('/createuser',[
    body('name','Enter a valid name').isLength({min:3}),
    body('email','Enter a valid email').isEmail(),
    body('password').isLength({min:5}),
-],(req,res)=>{
+],async (req,res)=>{
   // console.log(req.body);
   // const user = User(req.body);
   // user.save();
   // res.send(req.body);
+  // if there are errors then return the bad request and the errors
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({errors : errors.array()});
   }
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(sourabh => res.json(sourabh))
-    .catch(err => {console.log(err)
-    res.json({error: 'Please Enter a unique value for email',message : err.message})}
-    );
+  //check whether the email exits already 
+  try{
+    let user =  await User.findOne({email:req.body.email})
+    if(user){
+      return res.status(400).json({error : "Sorry a User with this Email already exists"})
+    }
+    user = await Userd.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    
+    // .then(sourabh => res.json(sourabh))
+    //   .catch(err => {console.log(err)
+    //   res.json({error: 'Please Enter a unique value for email',message : err.message})}
+    //   );
+    res.send(user);
+  }
+  catch(error){
+     console.error(error.message);
+     res.status(500).send("Some Error Occured");
+  }
 
 })
 
